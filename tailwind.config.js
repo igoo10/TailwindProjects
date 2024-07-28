9a5fe870f9445aadcbc3a24c4c2466c27010dd84
igoo10 <ighaloemmanuel3@gmail.com>
2024-07-28 23:22:36 +0100

    add my tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./HTML/index.html",
+    "./CSS/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
